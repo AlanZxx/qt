cb05a15d9b043249bb6e178de1b947e97441877f
potato <729422899@qq.com>
2020-01-08 16:47:09 +0800

    designer的配置 将pycharm添加designer和pyuic 添加第一个手写的Qwidget QutiApplication点击退出 添加总的文件

--- src/Detection/detection.py
+++ src/Detection/detection.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QApplication,QTextEdit,QDesktopWidget,QMessageBox,QWidget,QGridLayout,QLabel,QPushButton,QRadioButton,QFileDialog,QDialog
-from PyQt5.QtGui import QIcon,QPixmap
+from PyQt5.QtWidgets import QApplication,QListWidget,QListWidgetItem,QTextEdit,QMessageBox,QWidget,QGridLayout,QLabel,QPushButton,QRadioButton,QFileDialog,QDialog
+from PyQt5.QtGui import QIcon,QPixmap,QFont
 from PyQt5.QtCore import Qt,QDir,QSize
 import sys
+import glob
+import os
 
 # 0表示文件,1表示文件夹
 global file_or_folder_status
@@ -15,21 +17,29 @@ class MainWindow(QWidget):
         self.initListener()
 
     def initUI(self):
+        font = QFont("Microsoft YaHei", 20, 60);
+        # 第一个属性是字体（微软雅黑），第二个是大小，第三个是加粗（权重是75）
         desktop = QApplication.desktop()
         # self.setGeometry(300,300,250,150)
-        self.resize(desktop.width()*3/4, desktop.height()*3/4)
+        self.resize(int(desktop.width()*3/4),int(desktop.height()*3/4))
 
-        self.label_left = QLabel()
-        self.label_left.setText("检测文件夹结果详情")
+        self.label_left = QListWidget()
+        # self.label_left.setText("检测文件夹结果详情")
+        # self.label_left.setFont(font)
+        self.label_left.setWindowTitle("文件夹线下图片列表")
+
         self.label_middle1 = QLabel()
         self.label_middle1.setText("原图片/图片集")
+        self.label_middle1.setFont(font)
 
 
         self.label_middle2 = QLabel()
         # self.label_middle2.setPixmap(QPixmap("./../image/1.jpg"))
+        self.label_middle2.setFont(font)
         self.label_middle2.setText("结果图片/图片集")
 
-        self.label_right1 = QTextEdit()
+        self.label_right1 = QLabel()
+        self.label_right1.setFont(font)
         self.label_right1.setText("目标详情")
 
         self.label_right2 = QLabel()
@@ -49,8 +59,8 @@ class MainWindow(QWidget):
         self.select  = QPushButton("选择文件/文件夹")
         self.btn_send = QPushButton("开始检测")
         mainLayout = QGridLayout()
-        mainLayout.addWidget(self.label_left,0,0,8,3)
-        mainLayout.addWidget(self.status,8,0,2,3)
+        mainLayout.addWidget(self.label_left,0,0,6,3)
+        mainLayout.addWidget(self.status,6,0,5,3)
         mainLayout.addWidget(self.label_middle1,0,3,5,5)
         mainLayout.addWidget(self.label_middle2,5,3,5,5)
 
@@ -59,9 +69,10 @@ class MainWindow(QWidget):
         mainLayout.addWidget(self.radio1,7,8,1,1)
         mainLayout.addWidget(self.radio2,7,9,1,1)
         mainLayout.addWidget(self.select,8,8,1,2)
-        # mainLayout.addWidget(self.btn1_select_photo,8,8,1,1)
-        # mainLayout.addWidget(self.btn2_select_folder,8,9,1,1)
         mainLayout.addWidget(self.btn_send,9,8,1,2)
+        # path = r"E:\python_code\qt\src\image\1.jpg"
+        # self.update_display(self.label_middle2,path)
+        # self.update_display(self.label_middle1,path)
 
         self.setWindowTitle("目标检测主窗口")
         self.setWindowIcon(QIcon("./../image/excavator.ico"))
@@ -69,6 +80,9 @@ class MainWindow(QWidget):
 
     # 监听事件
     def initListener(self):
+        # 左上侧文件夹下文件的点击事件
+        self.label_left.itemClicked.connect(self.listItemClicked)
+
         # 两个radiobutton的事件
         self.radio1.toggled.connect(lambda :self.btnstate(self.radio1))
         self.radio2.toggled.connect(lambda :self.btnstate(self.radio2))
@@ -78,6 +92,25 @@ class MainWindow(QWidget):
         self.select.clicked.connect(self.loadImage)
     #     开始检测按钮
         self.btn_send.clicked.connect(self.startDetect)
+
+    def listItemClicked(self,item):
+        global path
+        file_path = path+'/'+item.text()
+        self.update_text(self.label_right2,file_path)
+        self.update_display(self.label_middle1,file_path)
+        # 字符串分割,然后寻找Alarm里面有没有对应的名字的文件，有就展出，没有就显示
+        # todo，将txt文件的结果进行解析
+        folder,fname = os.path.split(file_path)
+        new_path  = folder+"/"+"ssss.txt"
+        text = ""
+        if not os.path.exists(new_path):
+            new_path = file_path
+            text = "该图片暂无目标"
+
+
+        self.update_text(self.label_right1,text)
+        self.update_display(self.label_middle2,new_path)
+        # self.update_display(self.label_right1,get_photo_info())
     def startDetect(self):
         global file_or_folder_status
         global path
@@ -86,40 +119,71 @@ class MainWindow(QWidget):
         if path is None:
             self.showDialog("文件/夹路径为空")
         elif file_or_folder_status == 0:
-            self.status.append("\n"+"开始检测文件:"+path)
+            text = "\n"+"开始检测文件:"+path
+            self.add_status(self.status,text)
+            # --------------------------执行检测过程-----------------------------
             details = []
             for i in range(3):
                 detail = {}
-                detail["name"] = "x"+i
+                detail["name"] = "x"+str(i)
                 detail["x"] = i*10
                 detail["p"]= i+100
-                details[i]=detail
-            self.result_file(self.label_middle2, self.label_right2, path)
+                details.append(detail)
+            photo_path = path
+            self.result_file(self.label_middle2, self.label_right1, photo_path,details)
             # self.result(self.label_middle2,self.label_right1,details,path1=path)
         elif file_or_folder_status == 1:
             self.status.append("\n"+"开始检测文件夹:"+path)
-            # self.showDialog("开始检测文件夹"+path)
-            # self.result(self., text)
+            # 开始检测文件夹
+
         else:
             print("文件/文件夹状态异常")
     #     加载图片槽
+    def update_display(self,label,photo_path):
+        pixmap = QPixmap(photo_path)
+        sz = QSize(self.geometry().width()/2, self.geometry().width()/2)
+        # sz = QSize(label.width(), label.height())
+        label.setScaledContents(True)
+        # print(label.width(),label.height())
+        pixmap = pixmap.scaled(sz, Qt.KeepAspectRatio)
+        label.setPixmap(pixmap)
+
+    # 更新label标签，label是label名字，text是要更新的str
+    def update_text(self,label,text):
+        label.setText(text)
+
+    # 添加status标签内容，status是label名字，text是要更新的str
+    def add_status(self,status,text):
+        status.append(text)
+
+    # 选择图片或者文件夹
     def loadImage(self):
         global path
-        fname, _ = QFileDialog.getOpenFileName(self, "打开文件", '.', '图像文件(*.jpg *.png)')
-        path = fname
-        self.label_middle1.setScaledContents(True)
-        self.label_right2.setText(path)
-        self.status.append("已选择 "+path+" \n请开始检测...")
-        pixmap = QPixmap(fname)
-        sz = QSize(self.label_middle1.width(),self.label_middle1.height())
-        pixmap = pixmap.scaled(sz,Qt.KeepAspectRatio)
-        self.label_middle1.setPixmap(pixmap)
+        global  file_or_folder_status
+        if file_or_folder_status==1:
+            self.loadPath()
+        else:
+            fname, _ = QFileDialog.getOpenFileName(self, "打开文件", '.', '图像文件(*.jpg *.png)')
+            path = fname
+            self.update_display(self.label_middle1,path)
+            self.update_text(self.label_right2,path)
+            text = "已选择 " + path + " \n请开始检测..."
+            self.add_status(self.status,text)
 
+    # 选择文件夹
     def loadPath(self):
-        # 待完成
-        dlg = QFileDialog()
-        dlg.setFileMode(QFileDialog.AnyFile)
-        dlg.setFilter(QDir.Files)
+        global path
+        folder_path= QFileDialog.getExistingDirectory(self,"打开文件夹",'./')
+        path = folder_path
+        text = "已选择文件夹： " + folder_path + " \n请开始检测..."
+        self.add_status(self.status,text)
+        self.update_text(self.label_right2, folder_path)
+        files = os.listdir(folder_path)
+        for file in files:
+            self.label_left.addItem(file)
+            # text = text + file+"\n"
+        # self.label_left.setText(text)
+
     # QRadioButton事件
     def btnstate(self,btn):
         global file_or_folder_status
@@ -140,6 +204,7 @@ class MainWindow(QWidget):
                 # print(btn.text()+"is selected")
             # else:
                 # print(btn.text()+"is not selected")
+
     def showDialog(self,paramater):
         messageBox = QMessageBox()
         messageBox.warning(self,"警告!",paramater,QMessageBox.Yes,QMessageBox.Yes)
@@ -152,12 +217,24 @@ class MainWindow(QWidget):
         # dialog.setWindowModality(Qt.ApplicationModal)
         # dialog.exec_()
         # 检测结果展示
-    def result_file(self, pix,text,path1):
-        pixmap = QPixmap(path1)
-        pix.setScaledContents(True)
-        sz = QSize(pix.width(), pix.height())
-        pixmap = pixmap.scaled(sz, Qt.KeepAspectRatio)
-        pix.setPixmap(QPixmap(pixmap))
+
+    # 展示检测结果文件，更新状态图片ui，以及label
+    def result_file(self, pix,text,path,details):
+        self.update_display(self.label_middle2,path)
+        text.setText(self.getDetails(details))
+
+
+    def getDetails(self,details):
+        text = ""
+        # details = None
+        if details is None:
+            text = "这张图片没有检测到目标"
+            return text
+        for detail in details:
+            text=text+"object"
+            for key in detail.keys():
+                text = text+ "\t"+key+":"+str(detail[key])+"\n"
+        return text
 
     def result(self,pix,text,details,path1):
         pix.setPixmap(QPixmap(path1))
---
+++ test.py
@@ -0,0 +1,6 @@
+import  os
+path ="sss"
+
+item="222"
+file_path = os.path.join(path, item)
+print(file_path)
